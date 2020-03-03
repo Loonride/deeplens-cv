d9746532b42877ed3fad0aaa65bfd9eb1cd778de
Kirill Nagaitsev <knagaitsev1@gmail.com>
2020-03-03 14:56:13 -0600

    started switching to full manager

--- flask_server/server.py
+++ flask_server/server.py
@@ -8,8 +8,10 @@ sys.path.append('../')
 from deeplens.struct import *
 from deeplens.utils import *
 from deeplens.dataflow.map import *
-from deeplens.simple_manager.manager import *
-from deeplens.utils.testing_utils import *
+from deeplens.full_manager.full_manager import *
+from deeplens.full_manager.condition import Condition
+from deeplens.full_manager.full_video_processing import CropSplitter
+from deeplens.tracking.background import FixedCameraBGFGSegmenter
 
 from flask import Flask, request, render_template, Markup, abort
 from flask_uploads import UploadSet, configure_uploads
@@ -26,19 +28,25 @@ app.config['UPLOADED_VIDEOS_DEST'] = 'videos'
 videos = UploadSet('videos', ['mp4'])
 configure_uploads(app, videos)
 
-args = {'encoding': XVID, 'sample': 1.0, 'offset': 0, 'limit': 1000, 'batch_size': 1000}
+def do_filter(conn, video_name):
+    c = conn.cursor()
+    c.execute("SELECT clip_id FROM clip WHERE video_name = '%s'" % (video_name))
+    res = c.fetchall()
+    print(res[0])
+    return [cl[0] for cl in res]
 
-manager = SimpleStorageManager('test_store')
+args = {'encoding': XVID, 'size': -1, 'sample': 1.0, 'offset': 0, 'limit': 1000, 'batch_size': 500}
+
+manager = FullStorageManager(CustomTagger(FixedCameraBGFGSegmenter().segment, batch_size=500), CropSplitter(), 'test_store')
 
 def get_frames(name):
     try:
         def filter(header_data):
             return True
-        res = manager.get(name, filter, args)
+        res = manager.get(name, Condition(label='foreground', custom_filter=do_filter))
         vid = res[0]
         html = "";
         for frame in vid:
-            print(getFrameNumber(frame))
             ret, buff = cv2.imencode('.jpg', getFrameData(frame))
             b64 = base64.b64encode(buff)
             html += "<img src='data:image/jpg;base64," + b64.decode("utf-8") + "'>"
@@ -61,3 +69,13 @@ def upload():
     filename = videos.save(request.files['file'])
     manager.put('videos/' + filename, filename, args)
     return get_frames(filename)
+
+# @app.route("/names")
+# def names():
+#     conn = manager.conn
+#     cursor = conn.cursor()
+#     cursor.execute("SELECT video_name FROM clip")
+#     select_result = cursor.fetchall()
+#     names = set([cl[0] for cl in select_result])
+#     print(names)
+#     return ""
