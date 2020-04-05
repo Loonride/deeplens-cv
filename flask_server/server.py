bbd745ec39cfbeea06269430fa3761a9890d7402
Kirill Nagaitsev <knagaitsev1@gmail.com>
2020-04-05 14:30:12 -0500

    clear server data for cleanup

--- flask_server/server.py
+++ flask_server/server.py
@@ -3,6 +3,7 @@ import base64
 import random
 import os
 import sys
+import shutil
 from urllib.parse import quote
 sys.path.append('../')
 
@@ -20,12 +21,21 @@ from flask_uploads import UploadSet, configure_uploads
 # creates a Flask application, named app
 app = Flask(__name__)
 
-try:
-    os.mkdir('test_store')
-except:
-    pass
+store_dir = 'test_store'
+videos_dir = 'videos'
 
-app.config['UPLOADED_VIDEOS_DEST'] = 'videos'
+def clean_up():
+    if os.path.exists(store_dir):
+        shutil.rmtree(store_dir)
+    if os.path.exists(videos_dir):
+        shutil.rmtree(videos_dir)
+
+if not os.path.exists(store_dir):
+    os.mkdir(store_dir)
+if not os.path.exists(videos_dir):
+    os.mkdir(videos_dir)
+
+app.config['UPLOADED_VIDEOS_DEST'] = videos_dir
 videos = UploadSet('videos', ['mp4'])
 configure_uploads(app, videos)
 
@@ -36,7 +46,7 @@ def do_filter(conn, video_name):
     print(res[0])
     return [cl[0] for cl in res]
 
-args = {'encoding': XVID, 'size': -1, 'sample': 1.0, 'offset': 0, 'limit': 1000, 'batch_size': 500}
+args = {'frame_rate': 30, 'encoding': XVID, 'limit': 1000, 'sample': 1.0, 'offset': 0, 'batch_size': 500, 'num_processes': 4, 'background_scale': 1}
 
 # change the 3 parameters that get passed into the BGFG segmenter based on user input
 manager = FullStorageManager(CustomTagger(FixedCameraBGFGSegmenter().segment, batch_size=500), CropSplitter(), 'test_store', reuse_conn=False)
@@ -68,7 +78,6 @@ def get_clip_names():
 
 def create_frames_json(name):
     frames = get_frames(name)
-    print(frames)
     return '{"imgs":"' + quote(frames) + '","name":"' + name + '"}'
 
 @app.route("/")
